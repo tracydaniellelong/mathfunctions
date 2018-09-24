ce24a529c7aeeb992ab1a2e983aa973e357d61b1
tracydaniellelong <tracydaniellelong@gmail.com>
2018-09-24 14:18:45 -0400

    add test 2

--- add_testing.rb
+++ add_testing.rb
@@ -4,7 +4,10 @@ require_relative "addition.rb"
 class Add_test < Minitest::test
 	def test1
 		assert_equal(2, addition(1, 1))
-		
 	end
+	def test2
+		assert_equal(10, addition(3, 7))
+	end
+	
 end
 
