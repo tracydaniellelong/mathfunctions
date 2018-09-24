49e090821db33c7c102e17519f0bcdfb3a664bb3
tracydaniellelong <tracydaniellelong@gmail.com>
2018-09-24 15:08:46 -0400

    subtract test 1

--- subtract_test.rb
+++ subtract_test.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require_relative "subtraction.rb"
 
-class Add_test < Minitest::test
+class Subtract_test < Minitest::test
 	def test1
 		assert_equal(0, subtract(1, 1))
 	end
